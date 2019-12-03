c19eb64f4a9ce8e42036d49ebf716f874c93719b
haymanjoyce <haymanjoyce@gmail.com>
2019-12-03 22:11:36 +0000

    Developing classes in shapes module

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ print(style_a)
 rectangle_a = shapes.Rectangle(height=10, width=20, style=style_a)
 print(rectangle_a)
 
+frame_a = shapes.Frame()
+print(frame_a)
+
+
--- shapes.py
+++ shapes.py
@@ -1,3 +1,5 @@
+# svg values need to be in double quotes
+
 class Style:
 
     def __init__(self, fill=None, stroke=None, stroke_width=None, opacity=None):
@@ -28,4 +30,26 @@ class Rectangle:
         return f'<rect width={self.width} ' \
                f'height={self.height} ' \
                f'style="{self.style}"></rect>'
+
+
+class Frame:
+
+    def __init__(self, x=None, y=None, rx=None, ry=None, width=None, height=None,
+                 fill=None, padding=None, border_color=None, border_width=None, margin=None):
+
+        self.x = x
+        self.y = y
+        self.rx = rx
+        self.ry = ry
+        self.width = width
+        self.height = height
+
+    def __repr__(self):
+        # need border because inner and outer rounding
+        return f'<g transform="translate(100, 100)">' \
+               f'<!--Margin--><rect x="0" y="0" width="200" height="200" fill="grey" fill-opacity="40"></rect>' \
+               f'<!--Border--><rect x="20" y="20" width="160" height="160" fill="black"></rect>' \
+               f'<!--Padding--><rect x="30" y="30" width="140" height="140" fill="green" fill-opacity="40""></rect>' \
+               f'<!--Content--><rect x="40" y="40" width="120" height="120" fill="red" fill-opacity="40""></rect>' \
+               f'</g>'
 
