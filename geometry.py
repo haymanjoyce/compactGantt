baa2573f8f01418b8181ca858e9fa5042b113227
haymanjoyce <haymanjoyce@gmail.com>
2020-01-29 10:24:42 +0000

    Improvements to Geometry class

--- geometry.py
+++ geometry.py
@@ -5,47 +5,47 @@ import shapes
 @dataclass
 class Geometry:
 
-    # todo something not right about window geometry
+    # the SVG image to be rendered
+    image: str = str()
 
-    elements: str = str()
+    # todo you need to give the object the image size and then post init viewBox to match
 
-    # sets dimensions of SVG element
-    # has no effect on elements it contains
-    # GUI appears to ignore it (values do not seem to affect behaviour)
+    # sets top left corner of GUI window relative to display
+    GUI_x: int = 10
+    GUI_y: int = 100
+
+    # sets dimensions of GUI window
+    # advice is to select size based on display size (not image size)
+    GUI_width: int = 300
+    GUI_height: int = 300
+
+    # should set the dimensions of the root SVG element but seems to have no effect in GUI
     viewPort_width: int = None
     viewPort_height: int = None
 
-    # offsets viewBox
-    # a negative x value will offset it left causing SVG to apparently move right
+    # sets top left corner of viewBox
+    # a negative x value will seem to move the SVG image right
     viewBox_x: int = 0
     viewBox_y: int = 0
 
     # sets viewBox size in GUI
-    # below this size the SVG elements scale down but aspect ratio is maintained
-    # above this size the SVG elements stay the same and padding automatically takes up slack (centre aligned)
-    viewBox_width: int = 400
-    viewBox_height: int = 200
-
-    # sets top left corner of GUI window relative to display
-    GUI_x: int = 0
-    GUI_y: int = 0
-
-    # defines "padding" (my conception) between GUI window edge and SVG element edge
-    padding: int = field(default=200)
-
-    # sets dimensions of GUI window
-    GUI_width: int = field(init=False)
-    GUI_height: int = field(init=False)
+    # defines proportion of visible image on initial rendering
+    # the viewBox does not define size of image on screen
+    # a 100x100 will show that much of a 300x300 image scaled up to fit, say, a 600x600 window
+    # setting values to 0 will cause image to fill GUI window; aspect ration not preserved
+    # setting values to more than 0 will cause image to fill to shortest dimension of GUI window; aspect ratio preserved
+    # advice, for most applications, is to match size to image size
+    viewBox_width: int = 300
+    viewBox_height: int = 300
 
     def __post_init__(self):
-        self.GUI_width = self.viewBox_width + self.padding
-        self.GUI_height = self.viewBox_height + self.padding
+        pass
 
     def get_elements(self):
         return f'<?xml version="1.0" encoding="UTF-8" standalone="no"?> ' \
                f'<svg width="{self.viewPort_width}" height="{self.viewPort_height}" ' \
                f'viewBox="{self.viewBox_x} {self.viewBox_y} {self.viewBox_width} {self.viewBox_height}" ' \
                f'id="" version="1.1" overflow="auto"> ' \
-               f'{self.elements}' \
+               f'{self.image}' \
                f'</svg>'
 
