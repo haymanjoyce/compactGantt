c7ea926a1f7a3449a6f14f22434d2cf1080c2d8e
haymanjoyce <haymanjoyce@gmail.com>
2020-04-07 20:35:18 +0100

    Created all classes in rows module

--- rows.py
+++ rows.py
@@ -1,64 +1,80 @@
 """Module for rows, groups, and swimlanes"""
 
-# todo possible swimlane class
-# todo possible data column class
-# todo possible renaming of this module
+# todo possible renaming and redefining of this module
 
 from dataclasses import dataclass
 from shapes import Rect, Circle, Diamond, Text
 
 
 @dataclass
-class Task(Rect, Text):
-
-    id: int = int()
-    parent: int = int()  # parent row
-
-    def build_task(self):
-        pass
-
-    def get_task(self):
-        pass
-
-
-@dataclass
-class Milestone(Circle, Diamond, Text):
-
-    id: int = int()
-    parent: int = int()  # parent row
-
-    def build_milestone(self):
-        pass
-
-    def get_milestone(self):
-        pass
-
-
-@dataclass
-class Row(Rect):
-
-    id: int = 1
-    parent: int = int()  # parent group
-
-    row_padding: float = float()
-    task_margin: float = float()
-
-    def __post_init__(self):
-
-        if 0 < self.row_padding < (self.height * 0.5):
-            self.y += self.row_padding
-            self.height -= self.row_padding * 2
-
-
-@dataclass
-class Group(Text):
+class Base:
 
     id: int = int()
     parent: int = int()
 
-    def build_group(self):
-        pass
+
+@dataclass
+class Row(Base, Rect):
+
+    def get_row(self):
+
+        return f'{self.get_rect()}'
+
+
+@dataclass
+class TimeLine(Base, Rect):
+
+    def get_timeline(self):
+
+        return f'{self.get_rect()}'
+
+
+@dataclass
+class SwimLane(Base, Rect):
+
+    def get_swimlane(self):
+
+        return f'{self.get_rect()}'
+
+
+@dataclass
+class Task(Base, Rect, Text):
+
+    def get_task(self):
+
+        return f'{self.get_rect()} ' \
+               f'{self.get_text()}'
+
+
+@dataclass
+class Milestone(Base, Circle, Diamond, Text):
+
+    diamond: bool = True
+
+    def get_milestone(self):
+
+        if self.diamond:
+            pass
+        else:
+            pass
+
+        return f'{self.get_diamond()} ' \
+               f'{self.get_text()}'
+
+
+@dataclass
+class Group(Base, Rect, Text):
 
     def get_group(self):
-        pass
+
+        return f'{self.get_rect()} ' \
+               f'{self.get_text()}'
+
+
+@dataclass
+class Cell(Base, Rect, Text):
+
+    def get_cell(self):
+        return f'{self.get_rect()} ' \
+               f'{self.get_text()}'
 
