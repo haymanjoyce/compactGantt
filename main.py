b8ead453bac227118d4b72f73d4c1df3c16446d1
haymanjoyce <haymanjoyce@gmail.com>
2020-04-22 15:07:42 +0100

    Dropped Google sheets import

--- main.py
+++ main.py
@@ -30,23 +30,6 @@ from scales import Scale, Grid
 from mixins import *
 from shapes import Diamond, Text
 
-# EXTRACT DATA FROM GOOGLE SHEETS
-# https://www.twilio.com/blog/2017/02/an-easy-way-to-read-and-write-to-a-google-spreadsheet-in-python.html
-# use creds to create a client to interact with the Google Drive API
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-module_path = str(__file__)  # os.getcwd() was getting to wrong place
-cwd = os.path.dirname(module_path)
-client_secret = os.path.join(cwd, "client_secret.json")
-creds = ServiceAccountCredentials.from_json_keyfile_name(client_secret, scope)
-client = gspread.authorize(creds)
-# Find a workbook by name and open the first sheet
-# Make sure you use the right name here.
-sheet = client.open("data").sheet1
-print(client.list_spreadsheet_files())
-# Extract and print all of the values
-list_of_hashes = sheet.get_all_records()
-print(list_of_hashes)
-
 # TEMP
 today = date.toordinal(date.today()) - 2
 duration = 50
