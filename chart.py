a3d4075d0db64bb498fe97efc942fd354ced73c4
haymanjoyce <haymanjoyce@gmail.com>
2020-05-05 10:09:31 +0100

    Exploring Qt renderer

--- chart.py
+++ chart.py
@@ -9,23 +9,20 @@ from dataclasses import dataclass
 
 @dataclass
 class Chart:
-    """Handles image and creates images of chart in various formats"""
 
     chart: str = str()
 
     viewPort_width: int = 800
     viewPort_height: int = 600
-
     viewBox_x: int = -100
     viewBox_y: int = -100
-
     viewBox_width: int = 1600
     viewBox_height: int = 1200
 
     def get_svg(self):
         return f'<svg width="{self.viewPort_width}" height="{self.viewPort_height}" ' \
                f'viewBox="{self.viewBox_x} {self.viewBox_y} {self.viewBox_width} {self.viewBox_height}" ' \
-               f'id="" overflow="auto"> ' \
+               f'id="chart" overflow="auto"> ' \
                f'{self.chart}' \
                f'</svg>'
 
--- gui.py
+++ gui.py
@@ -3,15 +3,24 @@
 import sys
 from PySide2.QtWidgets import QApplication
 from PySide2.QtSvg import QSvgWidget
-from PySide2.QtCore import QByteArray
+from PySide2.QtCore import QByteArray, QRect
 
 
 def update_gui(svg='No SVG code found.'):
     gui = QApplication(sys.argv)
     window = QSvgWidget()
-    window.renderer().load(QByteArray(bytearray(svg, encoding='utf-8')))
+    contents = QByteArray(bytearray(svg, encoding='utf-8'))  # will also accept svg file
+    window.renderer().load(contents)
     geometry = 100, 100, 800, 600  # x, y, width, height
     window.setGeometry(*geometry)
+    print(window.sizeHint())
+    print(window.renderer())
+    print(window.renderer().matrixForElement('chart'))
+    print(window.renderer().boundsOnElement('chart'))  # Returns bounding rectangle of the item with the given id
+    # print(window.renderer().framesPerSecond())
+    print(window.renderer().viewBox())
+    print(window.renderer().viewBoxF())
+    window.renderer().setViewBox(QRect(width=200))
     window.show()
     sys.exit(gui.exec_())
 
