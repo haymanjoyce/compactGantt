4a6fbbc56514e1ce142ad4c5f61639092d2c25f7
haymanjoyce <haymanjoyce@gmail.com>
2020-07-29 12:07:09 +0100

    Removed ListCtrl from tabs module.

--- tabs.py
+++ tabs.py
@@ -2,11 +2,10 @@
 # todo ability to validate inputs and highlight if error or run health check
 # todo ability to export data
 # todo menu
-# todo automatically set column wx.LIST_FORMAT_RIGHT according to data type
 
 import wx
-import wx.lib.mixins.listctrl as listmix
-from pprint import pprint as pp
+import wx.grid as grid
+import wx.lib.gridmovers as gridmovers
 
 scale_fields = ('Start', 'Finish', 'Date Format')
 
@@ -21,77 +20,172 @@ scale_row_data = {
     1: ('5 Jan 20', '25 Jan 20', 'dd mm yy'),
 }
 
+identifiers = ['id', 'ds', 'sv', 'pr', 'pl', 'op', 'fx', 'ts']
 
-class Table(wx.ListCtrl, listmix.ListCtrlAutoWidthMixin, listmix.TextEditMixin):
+row_labels = ['Row1', 'Row2', 'Row3']
 
-    def __init__(self, parent, ID=wx.NewIdRef(), pos=wx.DefaultPosition, size=wx.DefaultSize, style=wx.LC_REPORT):
-        wx.ListCtrl.__init__(self, parent, ID, pos, size, style)
-        listmix.ListCtrlAutoWidthMixin.__init__(self)
-        listmix.TextEditMixin.__init__(self)
+col_labels = {'id': 'ID', 'ds': 'Description', 'sv': 'Severity',
+              'pr': 'Priority', 'pl': 'Platform', 'op': 'Opened?',
+              'fx': 'Fixed?', 'ts': 'Tested?'}
 
-        self.SetWindowStyle(wx.LC_REPORT | wx.LC_HRULES | wx.LC_VRULES | wx.LC_SORT_ASCENDING | wx.BORDER_NONE)
-
-        self.Bind(wx.EVT_LIST_BEGIN_LABEL_EDIT, self.on_being_label_edit)
-
-    def insert_columns(self, columns):
-        for column in columns:
-            index = columns.index(column)
-            self.InsertColumn(index, column, wx.LIST_FORMAT_LEFT)
-
-    def insert_rows(self, quantity):
-        for row in range(quantity):
-            self.InsertItem(row, 0)  # second argument is data (using 0 as default)
-
-    def import_by_column(self, data):
-        for column in range(self.GetColumnCount()):
-            values = data.get(self.GetColumn(column).GetText())
-            row = 0
-            for value in values:
-                self.SetItem(row, column, value)
-                row += 1
-
-    def import_by_row(self, data):
-        for row, values in data.items():
-            self.InsertItem(row, values[0])
-            column = 1
-            for value in values[1:]:
-                self.SetItem(row, column, value)
-                column += 1
-
-    def export_data(self):
-        data = {}
-        for row in range(self.GetItemCount()):
-            data[row] = [self.GetItem(row, col).GetText() for col in range(self.GetColumnCount())]
-        return data
-
-    def autosize_columns(self):
-        for col in range(self.GetColumnCount()):
-            self.SetColumnWidth(col, wx.LIST_AUTOSIZE)  # second argument is pixels
-
-    def set_cell_data(self, row, col, data):
-        wx.ListCtrl.SetItem(self, row, col, data)
-
-    def get_cell_data(self, row, col):
-        data = self.GetItem(row, col).GetText()
-        return data
-
-    def on_being_label_edit(self, event):
-        if event.Column == 1:
-            event.Veto()  # makes cell read only
-        else:
-            event.Skip()  # event available for another event handler
+data = [{'id': 1010,
+         'ds': "The foo doesn't bar",
+         'sv': "major",
+         'pr': 1,
+         'pl': 'MSW',
+         'op': 1,
+         'fx': 1,
+         'ts': 1
+         },
+        {'id': 1011,
+         'ds': "I've got a wicket in my wocket",
+         'sv': "wish list",
+         'pr': 2,
+         'pl': 'other',
+         'op': 0,
+         'fx': 0,
+         'ts': 0
+         },
+        {'id': 1012,
+         'ds': "Rectangle() returns a triangle",
+         'sv': "critical",
+         'pr': 5,
+         'pl': 'all',
+         'op': 0,
+         'fx': 0,
+         'ts': 0
+         }
+        ]
 
 
-class Tab(wx.Panel):
-    def __init__(self, parent):
-        wx.Panel.__init__(self, parent, -1, style=wx.WANTS_CHARS)
+class Table(grid.GridTableBase):
+    def __init__(self):
+        grid.GridTableBase.__init__(self)
 
-        self.table = Table(self)
+    # required methods for the wxPyGridTableBase interface
+    # must use wx format in order to override
 
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(self.table, wx.ID_ANY, wx.EXPAND)
-        self.SetSizer(sizer)
-        self.SetAutoLayout(True)
+    def GetNumberRows(self):
+        return len(data)
+
+    def GetNumberCols(self):
+        return len(identifiers)
+
+    def IsEmptyCell(self, row, col):
+        key = identifiers[col]
+        return not data[row][key]
+
+    def GetValue(self, row, col):
+        key = identifiers[col]
+        return str(data[row][key])
+
+    def SetValue(self, row, col, value):
+        key = identifiers[col]
+        data[row][key] = value
+
+    # some optional methods
+    # must use wx format in order to override
+
+    # called when the grid needs to display column labels
+    def GetColLabelValue(self, col):
+        key = identifiers[col]
+        return col_labels[key]
+
+    # called when the grid needs to display row labels
+    def GetRowLabelValue(self, row):
+        return row_labels[row]
+
+    # the physical moving of the cols and rows is left to the implementer
+
+    def move_column(self, frm, to):
+        tab = self.GetView()
+
+        if tab:
+            # move the identifiers
+            old = identifiers[frm]
+            del identifiers[frm]
+
+            if to > frm:
+                identifiers.insert(to - 1, old)
+            else:
+                identifiers.insert(to, old)
+
+            # Notify the grid
+            tab.BeginBatch()
+            msg = grid.GridTableMessage(
+                self, grid.GRIDTABLE_NOTIFY_COLS_DELETED, frm, 1
+            )
+
+            tab.ProcessTableMessage(msg)
+
+            msg = grid.GridTableMessage(
+                self, grid.GRIDTABLE_NOTIFY_COLS_INSERTED, to, 1
+            )
+
+            tab.ProcessTableMessage(msg)
+            tab.EndBatch()
+
+    def move_row(self, frm, to):
+        tab = self.GetView()
+        print(type(tab))
+
+        if tab:
+            # move the rowLabels and data rows
+            old_label = row_labels[frm]
+            old_data = data[frm]
+            del row_labels[frm]
+            del data[frm]
+
+            if to > frm:
+                row_labels.insert(to - 1, old_label)
+                data.insert(to - 1, old_data)
+            else:
+                row_labels.insert(to, old_label)
+                data.insert(to, old_data)
+
+            # notify the grid
+            tab.BeginBatch()
+
+            msg = grid.GridTableMessage(
+                self, grid.GRIDTABLE_NOTIFY_ROWS_DELETED, frm, 1
+            )
+
+            tab.ProcessTableMessage(msg)
+
+            msg = grid.GridTableMessage(
+                self, grid.GRIDTABLE_NOTIFY_ROWS_INSERTED, to, 1
+            )
+
+            tab.ProcessTableMessage(msg)
+            tab.EndBatch()
+
+
+class Tab(grid.Grid):
+    def __init__(self, parent, log=None):
+        grid.Grid.__init__(self, parent, -1)
+
+        table = Table()
+        self.SetTable(table, takeOwnership=True)  # not clear why do not make table into an attribute
+
+        # Enable Column moving
+        gridmovers.GridColMover(self)
+        self.Bind(gridmovers.EVT_GRID_COL_MOVE, self.OnColMove, self)
+
+        # Enable Row moving
+        gridmovers.GridRowMover(self)
+        self.Bind(gridmovers.EVT_GRID_ROW_MOVE, self.OnRowMove, self)
+
+    # Event method called when a column move needs to take place
+    def OnColMove(self, evt):
+        frm = evt.GetMoveColumn()  # Column being moved
+        to = evt.GetBeforeColumn()  # Before which column to insert
+        self.GetTable().move_column(frm, to)
+
+    # Event method called when a row move needs to take place
+    def OnRowMove(self, evt):
+        frm = evt.GetMoveRow()  # Row being moved
+        to = evt.GetBeforeRow()  # Before which row to insert
+        self.GetTable().move_row(frm, to)
 
 
 class Tabs(wx.Notebook):
@@ -103,17 +197,6 @@ class Tabs(wx.Notebook):
 
         self.AddPage(tab_1, "Tab 1")
         self.AddPage(tab_2, "Tab 2")
-
-        tab_1.table.insert_columns(scale_fields)
-        tab_1.table.insert_rows(2)
-        tab_1.table.import_by_column(scale_column_data)
-
-        tab_2.table.insert_columns(scale_fields)
-        tab_2.table.import_by_row(scale_row_data)
-
-        # self.insert_columns()
-        # self.insert_rows(len(scale_data))
-        # self.import_data(scale_data.items())
 
 
 class Window(wx.Frame):
